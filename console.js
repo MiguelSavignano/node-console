00f6108e04afa70ac689c30656f91fa8b336b1c8
migue.masx@gmail.com <migue.masx@gmail.com>
2016-05-25 00:08:04 +0200

    we can import all files in the folder models

--- console.js
+++ console.js
@@ -6,10 +6,7 @@ var replServer = repl.start({
   prompt: "node > "
 });
 
-var Developer = require("./models/developer")
-
 var normalizedPath = require("path").join(__dirname, "models");
-
 require("fs").readdirSync(normalizedPath).forEach(function(file_name) {
   var name = file_name.replace('.js','')
   replServer.context[name] = require("./models/" + name);
